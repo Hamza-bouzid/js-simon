22c7f7137734d4a1e660a7e59edad9a21cc3715d
Hamza Bouzid <hamzadeveloper1@gmail.com>
2021-12-22 15:09:25 +0100

    da completare

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
+const container = document.querySelector(".box");
+const randomArray = [];
+while (randomArray.length < 5) {
+  const randomNumber = Math.floor(Math.random() * 100 + 1);
+  if (!randomArray.includes(randomNumber)) {
+    randomArray.push(randomNumber);
+  }
+}
 
+container.innerHTML = randomArray;
+
+setTimeout(() => {
+  container.innerHTML = "Scrivi i numeri che hai appena visto";
+  setTimeout(() => {
+    let numeriInseriti = [];
+    let numeriCoretti = [];
+    let contatore = 0;
+    for (let i = 0; i < 5; i++) {
+      const numero = parseInt(prompt("Scrivi un numero di quelli visti"));
+
+      numeriInseriti.push(numero);
+    }
+
+    for (let i = 0; i < numeriInseriti.length; i++) {
+      if (randomArray.includes(numeriInseriti[i])) {
+        contatore++;
+        numeriCoretti.push(numeriInseriti[i]);
+      }
+    }
+
+    if(contatore == 0) {
+      container.innerHTML = `
+    <ul>
+      <li>Hai indovinato <span>${contatore}</span> numero/i</li>
+    </ul>
+    `;
+    } else {
+      container.innerHTML = `
+    <ul>
+      <li>Hai indovinato <span>${contatore}</span> numero/i</li>
+      <li>Numeri indovinati: <span>${numeriCoretti}</span></li>
+    </ul>
+    `;
+    }
+    console.log(numeriCoretti);
+  }, 1000);
+}, 3000);
