078ed0a4cb8c47bc0ef57e229174efe58c8f702d
Hamza Bouzid <hamzadeveloper1@gmail.com>
2021-12-25 11:24:50 +0100

    completato

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ while (randomArray.length < 5) {
   }
 }
 
-container.innerHTML = randomArray;
+container.innerHTML = `
+<ul>
+  <li>Hai 30 secondi per memorizzare questi numeri</li>
+  </br>
+  <li>${randomArray}</li>
+</ul>
+`;
 
 setTimeout(() => {
   container.innerHTML = "Scrivi i numeri che hai appena visto";
@@ -15,11 +21,15 @@ setTimeout(() => {
     let numeriInseriti = [];
     let numeriCoretti = [];
     let contatore = 0;
-    for (let i = 0; i < 5; i++) {
-      const numero = parseInt(prompt("Scrivi un numero di quelli visti"));
 
-      numeriInseriti.push(numero);
-    }
+    do {
+      const numero = parseInt(prompt("Scrivi un numero di quelli visti"));
+      if (!numeriInseriti.includes(numero)) {
+        numeriInseriti.push(numero);
+      } else {
+        alert("Hai gia inserito questo numero");
+      }
+    } while (numeriInseriti.length < 5);
 
     for (let i = 0; i < numeriInseriti.length; i++) {
       if (randomArray.includes(numeriInseriti[i])) {
@@ -28,7 +38,7 @@ setTimeout(() => {
       }
     }
 
-    if(contatore == 0) {
+    if (contatore == 0) {
       container.innerHTML = `
     <ul>
       <li>Hai indovinato <span>${contatore}</span> numero/i</li>
@@ -44,4 +54,4 @@ setTimeout(() => {
     }
     console.log(numeriCoretti);
   }, 1000);
-}, 4000);
+}, 3000);
